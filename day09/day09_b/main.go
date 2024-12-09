13fd25b711ba9859be538cb7773a109df4bee474
Vassilis Vassiliadis <vasiliadis.vasilis@gmail.com>
2024-12-09 18:17:13 +0000

    feat: optimize day09_b more
    I had it all wrong! We just need to look at each file exactly once.

--- day09/day09_b/main.go
+++ day09/day09_b/main.go
@@ -43,54 +43,49 @@ func solution(puzzle *util.Puzzle, logger *log.Logger) int {
 	// If there was, in the example we'd see 8888 move between 333 and 5555 here:
 	//    0099.111777244.333....5555.6666.....8888..
 
-	updated := true
-	for updated {
-	next_loop:
-		updated = false
+	// VV: This is actually a lie, it also keeps track of blocks skipped (i.e. those which are empty)
+	blocksMoved := 1
 
-		for idx := len(blocks) - 1; idx > 0; idx-- {
-			if blocks[idx].idx == -1 {
+	// VV: We need only try to move a block once!
+	for idx := len(blocks) - 1; idx > 0; {
+
+	next_loop:
+		idx := len(blocks) - blocksMoved
+		if idx == 0 {
+			break
+		}
+		blocksMoved++
+
+		if blocks[idx].idx == -1 {
+			goto next_loop
+		}
+
+		for moveToIdx := 1; moveToIdx < idx; moveToIdx++ {
+			if blocks[moveToIdx].idx != -1 {
 				continue
 			}
 
-			for moveToIdx := 1; moveToIdx < idx; moveToIdx++ {
-				if blocks[moveToIdx].idx != -1 {
-					continue
-				}
+			if blocks[moveToIdx].size == blocks[idx].size {
+				blocks[moveToIdx].idx = blocks[idx].idx
+				blocks[idx].idx = -1
 
-				if blocks[moveToIdx].size == blocks[idx].size {
-					blocks[moveToIdx].idx = blocks[idx].idx
-					blocks[idx].idx = -1
+				goto next_loop
+			} else if blocks[moveToIdx].size > blocks[idx].size {
+				remainder := blocks[moveToIdx].size - blocks[idx].size
 
-					updated = true
-					for i := idx + 1; i < len(blocks); i++ {
-						if blocks[i].idx != -1 {
-							goto next_loop
-						}
-					}
-					// VV: if we got to this point everything past idx is empty!
+				blocks[moveToIdx].idx = blocks[idx].idx
+				blocks[moveToIdx].size = blocks[idx].size
 
-					blocks = blocks[0:idx]
-					goto next_loop
-				} else if blocks[moveToIdx].size > blocks[idx].size {
-					remainder := blocks[moveToIdx].size - blocks[idx].size
+				blocks[idx].idx = -1
 
-					blocks[moveToIdx].idx = blocks[idx].idx
-					blocks[moveToIdx].size = blocks[idx].size
+				newBlocks := []Block{}
 
-					blocks[idx].idx = -1
+				newBlocks = append(newBlocks, blocks[:moveToIdx+1]...)
+				newBlocks = append(newBlocks, Block{size: remainder, idx: -1})
+				newBlocks = append(newBlocks, blocks[moveToIdx+1:]...)
 
-					newBlocks := []Block{}
-
-					newBlocks = append(newBlocks, blocks[:moveToIdx+1]...)
-					newBlocks = append(newBlocks, Block{size: remainder, idx: -1})
-					newBlocks = append(newBlocks, blocks[moveToIdx+1:]...)
-
-					blocks = newBlocks
-
-					updated = true
-					goto next_loop
-				}
+				blocks = newBlocks
+				goto next_loop
 			}
 		}
 	}
